f7e4b39ad1cba09cad4bfa193e95a6e83b7062bc
morbidflames <tarunsg1106@gmail.com>
2026-01-01 22:54:51 +0530

    Implemented many window features

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,18 @@
 import dayjs from 'dayjs';
 import { navIcons, navLinks } from '#constants/index.js';
+import useWindowStore from "#store/window.js";
 
 
 const Navbar = () => {
+    const { openWindow } = useWindowStore();
   return (
     <nav>
       <div>
         <img src="/images/logo.svg" alt="logo" />
         <p className="font-bold">Tarun's Portfolio</p>
         <ul>
-          {navLinks.map(({ id, name}) => (
-            <li key={id}>
+          {navLinks.map(({ id, name, type}) => (
+            <li key={id} onClick={() => openWindow(type)}>
               <p>{name}</p>
             </li>
           ))}
---
+++ src/windows/Contact.jsx
@@ -0,0 +1,45 @@
+import WindowWrapper from "#hoc/WindowWrapper.jsx";
+import {socials} from "#constants";
+import {WindowControls} from "#components";
+
+const Contact = () => {
+    return (
+        <>
+            <div id="window-header">
+                <WindowControls target="contact" />
+                <h2>Contact Me</h2>
+            </div>
+
+            <div className="p-5 space-y-5">
+                <img
+                    src="/images/adrian.jpg"
+                    alt="Tarun"
+                    className="w-20 rounded-full"
+                />
+
+                <h3>Let's Connect!</h3>
+                <p>I'm open to discussing new ideas, tackling challenges, or engaging in technical conversations</p>
+
+                <ul>
+                    {socials.map(({ id, bg, link, icon, text}) => (
+                         <li key={id} style={{ backgroundColor: bg}}>
+                             <a
+                                 href={link}
+                                 target="_blank"
+                                 rel="noopener noreferrer"
+                                 title={text}
+                             >
+                                 <img src={icon} alt={text} className="size-5" />
+                                 <p>{text}</p>
+                             </a>
+                         </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    );
+};
+
+const ContactWindow = WindowWrapper(Contact, 'contact');
+
+export default ContactWindow;
